9a2d0a8eae7f74e4740305ba6e9c5315624fd77e
kuminoffV <108672745+kuminoffV@users.noreply.github.com>
2024-05-14 17:53:04 +0700

    Update 3task.c

--- 3task.c
+++ 3task.c
@@ -1,32 +1,27 @@
 #include <stdio.h>
-#include <sched.h>
 #include <string.h>
-#include <unistd.h>
 
 #define MAX_LENGTH 32
 
 void change_third_byte()
 {
-    int number = 0, number_replace = 0, eindian = 0;
+    int number = 0;
     unsigned char third_byte;
 
-    printf("\nenter number: ");
+    printf("\nEnter a number: ");
     scanf("%d", &number);
 
-    unsigned char *ptr = (unsigned char *)&number;
+    unsigned char *ptr = (unsigned char *)&number + 2;
 
-    ptr += 2;
-
-    printf("\nnumber byte:\n");
+    printf("\nEnter the new value for the third byte: ");
     scanf("%hhu", &third_byte);
 
     *ptr = third_byte;
 
-    ptr -= 2;
-
+    printf("\nUpdated number:\n");
     for (int i = 0; i < 4; i++)
     {
-        printf("address = %p \t  value = %d \t i = %d \n", ptr + i, *(ptr + i), i + 1);
+        printf("address = %p \t  value = %d \t i = %d \n", ptr - 2 + i, *(ptr - 2 + i), i + 1);
     }
 }
 
@@ -38,94 +33,48 @@ void changing_code()
     float y = 6.0;
     printf("y = %f\n", y);
 
-    float *xp = &y;
-    // float *xp = &x+1; а можно было просто сделать так)
+    float *xp = &x; // Only this line changed
     float *yp = &y;
 
-    printf("Результат: %f\n", *xp + *yp);
+    printf("Result: %f\n", *xp + *yp);
 }
 
 void array()
 {
-    int array_10[10] = {
-        10,
-        20,
-        30,
-        40,
-        50,
-        60,
-        70,
-        80,
-        90,
-        100};
+    int array_10[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
     int *array = array_10;
 
     for (int i = 0; i < 10; i++)
     {
-        printf("address = %p \t value = %d \t i = %d \n", array + i, *(array + i), i);
+        printf("address = %p \t value = %d \t i = %d \n", (void *)(array + i), *(array + i), i);
     }
 }
 
 void substring()
 {
-    char str[32];
-    char str2[32];
+    char str[MAX_LENGTH];
+    char str2[MAX_LENGTH];
     char *pstr;
 
-    while (getchar() != '\n')
-        ;
+    printf("\nEnter your string (up to %d characters):\n", MAX_LENGTH - 1);
+    scanf("%s", str);
 
-    printf("\nEnter your string (up to 31 characters):\n");
-    fgets(str, sizeof(str), stdin);
-    str[strcspn(str, "\n")] = '\0';
-
-    printf("\nComparison string (up to 31 characters):\n");
-    fgets(str2, sizeof(str2), stdin);
-    str2[strcspn(str2, "\n")] = '\0';
+    printf("\nEnter comparison string (up to %d characters):\n", MAX_LENGTH - 1);
+    scanf("%s", str2);
 
     pstr = strstr(str, str2);
 
     if (pstr == NULL)
-        printf("Pointer = NULL!\n");
+        printf("Substring not found!\n");
     else
-        printf("\nstrstr != 0 \nSo.Addr: %p \nMatch found in %zu symbol\n\n", (void *)pstr, strlen(str2));
+        printf("\nSubstring found at address: %p \nLength: %zu\n\n", (void *)pstr, strlen(str2));
 }
 
 int main()
 {
-    // 1. Поменять в целом положительном числе (типа int) значение
-    // третьего байта на введенное пользователем число (изначальное
-    // число также вводится с клавиатуры) через указатель (не применяя
-    // битовые операции).
-
-    // 2. В приведенном ниже коде измените только одну строку (помеченную),
-    // чтобы напечатать “Результат: 12.0”.
-
-    //     #include <stdio.h>
-    // int main(void)
-    // {
-    // float x = 5.0;
-    // printf("x = %f, ", x);
-    // float y = 6.0;
-    // printf("y = %f\n", y);
-    // float *xp = &x; // TODO: отредактируйте эту строку, и только правую часть
-    // уравнения
-    // float *yp = &y;
-    // printf("Результат: %f\n", *xp + *yp);
-    // }
-
-    // 3. Напишите программу, в которой определите массив типа int (размер
-    // массива 10 элементов от 1 до 10). Используйте указатель для вывода элементов
-    // массива на консоль (применять можно только арифметику указателей, а не индексы).
-
-    // 4. Напишите программу, которая ищет введенной строке (с клавиатуры)
-    // введенную подстроку (с клавиатуры) и возвращает указатель на начало
-    // подстроки, если подстрока не найдена в указатель записывается NULL.
-    // В качестве срок использовать статические массивы
-
     int task_num;
 
-    printf("Select task from 1-4: ");
+    printf("Select a task from 1-4: ");
     scanf("%d", &task_num);
 
     switch (task_num)
@@ -146,7 +95,7 @@ int main()
         break;
 
     default:
-        printf("Task number %u not found\n", task_num);
+        printf("Task number %d not found\n", task_num);
     }
 
     return 0;
